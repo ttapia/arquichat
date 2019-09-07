ec1f513dbbde9f4dc6be13acf4634518f4b46c7f
Ubuntu <ubuntu@ip-172-31-13-254.sa-east-1.compute.internal>
2019-09-07 00:59:58 +0000

    fixed char limit in text messages

--- chat/models.py
+++ chat/models.py
@@ -3,5 +3,5 @@ from django.utils.timezone import now
 
 class Message(models.Model):
     author = models.CharField(max_length=50)
-    text = models.CharField(max_length=140)
+    text = models.CharField(max_length=1000)
     pub_date = models.DateTimeField(default=now, editable=False);
